db4359d01ac8dc76561f153ec0d49a1fb8f25af3
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-03 09:51:44 +0100

    Header completato

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -66,12 +66,12 @@ export default {
     <header>
         <div class="container">
             <a href="#">
-                <img :src="logo" alt="">
+                <img :src="logo" alt="Logo">
             </a>
 
             <nav>
-                <ul v-for="link in links">
-                    <li><a href="#">{{ link.text }}</a></li>
+                <ul>
+                    <li v-for="link in links"><a href="#">{{ link.text }}</a></li>
                 </ul>
             </nav>
         </div>
@@ -85,17 +85,34 @@ header {
     height: 95px;
     padding: 10px 0;
 
-    img {
-        height: 80px;
+    // Debug
+    border: 1px solid black;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
-}
 
-.container {
-    display: flex;
-    justify-content: space-between;
-}
+    img {
+        height: 65px;
+    }
 
-ul {
-    list-style: none;
+    ul {
+        list-style: none;
+        height: 100%;
+        display: flex;
+
+        li a {
+            text-decoration: none;
+            padding: 0 10px;
+            font-size: 13px;
+            color: #5f605e;
+
+            &.active {
+                border-bottom: 1px solid #0282f9;
+            }
+        }
+    }
 }
 </style>
