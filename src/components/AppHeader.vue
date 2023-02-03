873a70bb2066caf1b67b9d8f75650621c493a201
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-03 10:20:34 +0100

    Completato Footer

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -85,9 +85,6 @@ header {
     height: 95px;
     padding: 10px 0;
 
-    // Debug
-    border: 1px solid black;
-
     .container {
         display: flex;
         justify-content: space-between;
@@ -99,15 +96,12 @@ header {
     }
 
     ul {
-        list-style: none;
-        height: 100%;
         display: flex;
 
         li a {
-            text-decoration: none;
             padding: 0 10px;
-            font-size: 13px;
-            color: #5f605e;
+            font-size: $font-medium;
+            color: $text-nav;
 
             &.active {
                 border-bottom: 1px solid #0282f9;
